6450c94f9f6d37b540b2517c39bc8d60a874ceb3
Iván Fischer <ivanfischer76@gmail.com>
2021-08-18 19:57:02 -0300

    Revisión final-18-09-2021

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <!--<img alt="Vue logo" src="./assets/logo.png">-->
   <Indecision/>
 </template>
 
 <script>
-// import Counter from './components/Counter.vue'
 import Indecision from './components/Indecision.vue'
 
 export default {
   name: 'App',
   components: {
-    // Counter,
 	Indecision
   }
 }
